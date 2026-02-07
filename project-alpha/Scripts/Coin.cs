1b42daf07a56616ff677e543cea259d7bf41c6fc
Piotr <pm.kupilas@gmail.com>
2026-02-07 17:27:36 +0900

    [ProjectAlpha] Added music and sfx.

--- project-alpha/Scripts/Coin.cs
+++ project-alpha/Scripts/Coin.cs
@@ -2,14 +2,22 @@ using Godot;
 
 public partial class Coin : Area2D
 {
+    private AnimationPlayer animationPlayer = null;
+    private string PickUpAnimationName = "PickUpAnimation";
+
+    public override void _Ready()
+    {
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
     private void OnBodyEntered(Node2D body)
     {
         AddPoint();
-        QueueFree();
     }
 
     private void AddPoint()
     {
         GameManager.Instance.AddPoint();
+        animationPlayer.Play(PickUpAnimationName);
     }
 }
